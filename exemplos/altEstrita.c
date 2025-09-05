ff636cd82ac4fe2aeae5d940d1a7186fd5cf8071
Jeferson José Baqueta <42413563+jjbaqueta@users.noreply.github.com>
2025-09-05 12:33:45 -0300

    Update altEstrita.c

--- exemplos/altEstrita.c
+++ exemplos/altEstrita.c
@@ -12,9 +12,14 @@
 int soma = 0;
 int turno = 0;
 
-// Variáveis para contagem de tempo
-clock_t inicio, fim;
+// Variáveis para contagem de tempo REAL
+struct timespec inicio, fim;
 double tempo;
+
+// Variáveis para contagem de tempo CPU
+clock_t inicioCPU, fimCPU;
+double tempoCPU;
+
 
 // Diretiva de entrada
 void enter(long id){
@@ -47,7 +52,8 @@ int main() {
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
     // Contagem do tempo é iniciada
-    inicio = clock();
+    clock_gettime(CLOCK_MONOTONIC, &inicio);
+    inicioCPU = clock();
 
     // Criando as threads
     for (long i = 0; i < NUM_THREADS; i++) {
@@ -66,12 +72,19 @@ int main() {
     }
 
     // Contagem do tempo é finalizada
-    fim = clock();
+    clock_gettime(CLOCK_MONOTONIC, &fim);
+    fimCPU = clock();
 
     // Exibe o resultado esperado
     printf("Soma esperada: %d - Soma obtida: %d\n", NUM_THREADS * NUM_ITERACOES, soma);
-    tempo = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+
+    // Calcula o tempo decorrido em segundos
+    tempo = (fim.tv_sec - inicio.tv_sec);
+    tempo += (fim.tv_nsec - inicio.tv_nsec) / 1000000000.0;
     printf("Tempo de execução: %.2f segundos\n", tempo);
+
+    tempoCPU = ((double)(fimCPU - inicioCPU)) / CLOCKS_PER_SEC;
+    printf("Tempo de CPU: %.2f segundos\n", tempoCPU);
 
     pthread_attr_destroy(&attr);
     pthread_exit(NULL);
